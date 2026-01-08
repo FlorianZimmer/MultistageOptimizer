b1ad14e79be32f6114ef1ffd98df84faa35bff1c
Florian Zimmer <florian.zimmer99@gmail.com>
2026-01-08 21:21:14 +0100

    build: simplify Windows resources
    - Keep single resource header as resource.h
    
    - Update MultistageOptimizer.rc includes

--- resource.h
+++ resource.h
@@ -1,12 +1,14 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Multistage_optimizer_0.1_CPP.rc
+// Von Microsoft Visual C++ generierte Includedatei.
+// Verwendet durch MultistageOptimizer.rc
+//
+#define IDI_ICON1                       101
 
-// Nächste Standardwerte für neue Objekte
+// Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
